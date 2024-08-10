b55455080cfed7b5b00183bebce40805d9aec24f
AnikiRudo5017 <nguyenvu5017@gmail.com>
2024-08-10 17:19:39 +0700

    .

--- Assets/Script/Phong_Thu/Linh_thu.cs
+++ Assets/Script/Phong_Thu/Linh_thu.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 public class Linh_thu : MonoBehaviour
 {
     [SerializeField] Animator anim;
+    public float detectionRange = 10f;
 
     private void Start()
     {
         anim = this.GetComponent<Animator>();
         StartCoroutine(SpawnLinhLuc()); 
     }
-    private void Update()
-    {
-        
-    }
+    
     private IEnumerator SpawnLinhLuc()
     {
         while (true)
@@ -24,5 +22,9 @@ public class Linh_thu : MonoBehaviour
             yield return new WaitForSeconds(1f);
             anim.SetTrigger("Idle");
         }
+    }
+    private void Tang_Linh_Luc()
+    {
+        //Tang_Linh_Luc += detectionRange;
     }
 }
